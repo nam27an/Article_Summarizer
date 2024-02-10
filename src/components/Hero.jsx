644e349f4ce873fc2c6108cf0338b2f888fb36d1
naman2703 <namansharma2703@gmail.com>
2024-02-10 10:11:13 +0530

    Article Summarizer

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import { logo } from '../assets';
+
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='Logo' className='w-28 object-contain' />
+
+        <button type='button' onClick={() => window.open('https://github.com/naman2703')} className='black_btn'>
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden'/>
+        <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className='desc'>
+        Simplify your reading with Sumz AI, an open-source Article summarizer that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  )
+}
+
+export default Hero
